6972b5ccf35d830e77aa366d80ff6bd2349dc8d7
Lisa Hare <hare.lisa@gmail.com>
2018-06-06 10:01:45 -0700

    initial commit

---
+++ BankApp/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+    class Account
+    {
+        //properties can be written by prop -tab-tab
+        public int AccountNumber { get; set; }
+        public char EmailAddress { get; set; }
+        public double Balance { get; set; }
+        public string AccountType { get; set; }
+    }
+}
